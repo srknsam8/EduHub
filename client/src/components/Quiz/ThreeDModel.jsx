03081f89592d546d59278476ab6e934db0084601
Anish-Parkhi <anishparkhi03@gmail.com>
2024-04-26 19:59:36 +0530

    feat: added 3d model

--- client/src/components/Quiz/ThreeDModel.jsx
+++ client/src/components/Quiz/ThreeDModel.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import styles from './ThreeDModel.module.css';
 
@@ -14,21 +14,19 @@ function GLTFRenderer() {
       75,
       container.clientWidth / container.clientHeight,
       0.1,
-      1000,
+      1000
     );
     const renderer = new THREE.WebGLRenderer({ alpha: true });
     const loader = new GLTFLoader();
 
-    camera.position.set(1, 1, 5); // Adjusted camera position
+    camera.position.set(1, 1, 19);
     renderer.setSize(container.clientWidth, container.clientHeight);
     renderer.setClearAlpha(0);
     container.appendChild(renderer.domElement);
 
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true; // Enable damping for smoother camera movements
-    controls.dampingFactor = 0.25; // Adjust damping factor if necessary
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
     directionalLight.position.set(1, 1, 1).normalize();
     scene.add(directionalLight);
 
@@ -39,9 +37,21 @@ function GLTFRenderer() {
     scene.add(hemisphereLight);
 
     let model;
-    loader.load('src/public/scene.gltf', (gltf) => { // Adjusted path to GLTF model
+    loader.load('src/public/scene.gltf', (gltf) => {
       model = gltf.scene;
       model.scale.set(0.1, 0.1, 0.1);
+
+      model.traverse((child) => {
+        if (child.isMesh) {
+          const { material } = child;
+          const texturePath =
+            'src/public/textures/thairoid01lungh_part02_baseColor.jpeg';
+          const texture = new THREE.TextureLoader().load(texturePath);
+          material.map = texture;
+
+          material.side = THREE.DoubleSide;
+        }
+      });
 
       scene.add(model);
     });
